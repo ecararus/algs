e51bf606d95f704aec5886e2bfe3daf2b946923c
Eugeniu <eugeniu@fsbtech.com>
2024-07-31 15:15:51 +0100

    Add more algs

--- src/main/java/com/ecararus/algs/stringManipulation/LongestPalindromicSubstring.java
+++ src/main/java/com/ecararus/algs/stringManipulation/LongestPalindromicSubstring.java
@@ -12,5 +12,49 @@ package com.ecararus.algs.stringManipulation;
  */
 public class LongestPalindromicSubstring {
 
+    public String findLongestPalindromicSubstring(String s) {
+        int n = s.length();
+        if (n == 0) return "";
+
+        boolean[][] dp = new boolean[n][n];
+        int maxLength = 1;
+        int start = 0;
+
+        // All substrings of length 1 are palindromes
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = true;
+        }
+
+        // Check for substrings of length 2
+        for (int i = 0; i < n - 1; i++) {
+            if (s.charAt(i) == s.charAt(i + 1)) {
+                dp[i][i + 1] = true;
+                start = i;
+                maxLength = 2;
+            }
+        }
+
+        // Check for substrings of length greater than 2
+        for (int length = 3; length <= n; length++) {
+            for (int i = 0; i < n - length + 1; i++) {
+                int j = i + length - 1;
+                if (dp[i + 1][j - 1] && s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = true;
+                    if (length > maxLength) {
+                        start = i;
+                        maxLength = length;
+                    }
+                }
+            }
+        }
+
+        return s.substring(start, start + maxLength);
+    }
+
+    public static void main(String[] args) {
+        LongestPalindromicSubstring lps = new LongestPalindromicSubstring();
+        String s = "babad";
+        System.out.println("Longest palindromic substring: " + lps.findLongestPalindromicSubstring(s)); // Output: "bab" or "aba"
+    }
 
 }
