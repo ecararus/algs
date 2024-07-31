4cbba38ab422bd9d7f6db7ad3cf733f88c4ac8ef
Eugeniu <eugeniu@fsbtech.com>
2024-07-31 11:03:38 +0100

    Add more algs

--- src/main/java/com/ecararus/algs/stringManipulation/LongestCommonSubsequence.java
+++ src/main/java/com/ecararus/algs/stringManipulation/LongestCommonSubsequence.java
@@ -1,4 +1,16 @@
 package com.ecararus.algs.stringManipulation;
 
+/**
+ * Find the longest subsequence common to two strings.
+ *
+ * Plan:
+ * 1. Create a method that takes two strings as input.
+ * 2. Create a 2D array to store the length of the longest common subsequence.
+ * 3. Loop through the strings and compare the characters.
+ * 4. If the characters match, increment the length of the longest common subsequence.
+ * 5. If the characters do not match, take the maximum of the previous longest common subsequence.
+ * 6. Return the length of the longest common subsequence.
+ *
+ */
 public class LongestCommonSubsequence {
 }
--- src/main/java/com/ecararus/algs/stringManipulation/LongestPalindromicSubstring.java
+++ src/main/java/com/ecararus/algs/stringManipulation/LongestPalindromicSubstring.java
@@ -1,4 +1,14 @@
 package com.ecararus.algs.stringManipulation;
 
+/**
+ * Find the longest substring that is a palindrome.
+ *
+ * Plan:
+ * 1. Create a method that takes a string as input.
+ * 2. Create a variable to store the longest palindrome.
+ * 3. Loop through the string and check for palindromes.
+ * 4. If a palindrome is found, update the longest palindrome.
+ * 5. Return the longest palindrome.
+ */
 public class LongestPalindromicSubstring {
 }
