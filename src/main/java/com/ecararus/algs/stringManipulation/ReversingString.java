884177e0274df0956e78f5352507ab8328a95168
Eugeniu <eugeniu@fsbtech.com>
2024-07-31 11:54:42 +0100

    Add more algs

--- src/main/java/com/ecararus/algs/stringManipulation/ReversingString.java
+++ src/main/java/com/ecararus/algs/stringManipulation/ReversingString.java
@@ -12,6 +12,17 @@ package com.ecararus.algs.stringManipulation;
  */
 public class ReversingString {
 
+    public String reverseString(String str) {
+        StringBuilder reversed = new StringBuilder();
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed.append(str.charAt(i));
+        }
+        return reversed.toString();
+    }
 
-
+    public static void main(String[] args) {
+        ReversingString reverser = new ReversingString();
+        String testStr = "hello";
+        System.out.println("Reversed string: " + reverser.reverseString(testStr)); // Output: "olleh"
+    }
 }
