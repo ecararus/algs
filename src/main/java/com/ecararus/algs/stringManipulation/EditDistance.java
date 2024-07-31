b479047993e8eab8290476c60d9a92ed721ff99b
Eugeniu <eugeniu@fsbtech.com>
2024-07-31 12:26:27 +0100

    Add more algs

--- src/main/java/com/ecararus/algs/stringManipulation/EditDistance.java
+++ src/main/java/com/ecararus/algs/stringManipulation/EditDistance.java
@@ -11,4 +11,36 @@ package com.ecararus.algs.stringManipulation;
  * 4. Return the value in the bottom-right corner of the 2D array.
  */
 public class EditDistance {
+
+    public int calculateEditDistance(String str1, String str2) {
+        int m = str1.length();
+        int n = str2.length();
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = i;
+        }
+        for (int j = 0; j <= n; j++) {
+            dp[0][j] = j;
+        }
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = 1 + Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1]));
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+
+    public static void main(String[] args) {
+        EditDistance editDistance = new EditDistance();
+        String str1 = "kitten";
+        String str2 = "sitting";
+        System.out.println("Edit distance between \"" + str1 + "\" and \"" + str2 + "\": " + editDistance.calculateEditDistance(str1, str2)); // Output: 3
+    }
 }
