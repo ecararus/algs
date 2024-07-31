b79f01bce061e8f17766c09772bf15356db30f02
Eugeniu <eugeniu@fsbtech.com>
2024-07-31 12:04:10 +0100

    Add more algs

--- src/main/java/com/ecararus/algs/stringManipulation/CountVowelsConsonants.java
+++ src/main/java/com/ecararus/algs/stringManipulation/CountVowelsConsonants.java
@@ -11,4 +11,28 @@ package com.ecararus.algs.stringManipulation;
  * 5. Return the count of vowels and consonants.
  */
 public class CountVowelsConsonants {
+
+    public int[] countVowelsAndConsonants(String str) {
+        int vowels = 0;
+        int consonants = 0;
+        String vowelsList = "aeiouAEIOU";
+
+        for (char c : str.toCharArray()) {
+            if (Character.isLetter(c)) {
+                if (vowelsList.indexOf(c) != -1) {
+                    vowels++;
+                } else {
+                    consonants++;
+                }
+            }
+        }
+        return new int[]{vowels, consonants};
+    }
+
+    public static void main(String[] args) {
+        CountVowelsConsonants counter = new CountVowelsConsonants();
+        String testStr = "hello world";
+        int[] counts = counter.countVowelsAndConsonants(testStr);
+        System.out.println("Vowels: " + counts[0] + ", Consonants: " + counts[1]); // Output: Vowels: 3, Consonants: 7
+    }
 }
